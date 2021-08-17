920301597beed8e9beddc6dae52a33f8e72a4c8f
yukisat <s.yuuki0120@gmail.com>
2021-08-17 21:45:09 +0900

    1時間以内にスナップショットが作成された場合、スクリプトを実行しない処理を追加

--- ec2-snapshot.sh
+++ ec2-snapshot.sh
@@ -14,4 +14,19 @@ EC2_VolumeId=$( \
     --output text \
 )
 
-aws ec2 create-snapshot --volume-id ${EC2_VolumeId} --tag-specifications 'ResourceType="snapshot",Tags=[{Key="Name",Value='$MyInstance'},{Key="Auto_SS",Value="true"}]' --description "Auto snapshot at root login"
+## 1時間前のスナップショットが存在する場合作成しない
+TARGET_DATE=$(date "+%Y-%m-%dT%H:%M" -u -d '-1 hours')
+NOW_DATE=$(date "+%Y-%m-%dT%H:%M" -u)
+
+Snapshot=$(aws ec2 describe-snapshots \
+  --filters "Name=tag:Auto_SS,Values=true" \
+  --region ap-northeast-1 \
+  --query "Snapshots[?(StartTime>='$TARGET_DATE') && (StartTime<='$NOW_DATE')].[SnapshotId,StartTime]" \
+  --output text | head -n1)
+
+if [ -z "$Snapshot" ]; then
+    aws ec2 create-snapshot --volume-id ${EC2_VolumeId} --tag-specifications 'ResourceType="snapshot",Tags=[{Key="Name",Value='$MyInstance'},{Key="Auto_SS",Value="true"}]' --description "Auto snapshot at root login" 2>>./snapshot.log
+    echo "Automatic snapshot was performed"
+  else
+    echo "Automatic snapshot does not run"
+fi

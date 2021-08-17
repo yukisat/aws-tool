d0c0e921844f4f191754487f4d9c75cdd164a9ac
yukisat <s.yuuki0120@gmail.com>
2021-08-17 21:58:49 +0900

    ログの出力先を変更

--- ec2-snapshot.sh
+++ ec2-snapshot.sh
@@ -4,6 +4,7 @@ set -ue
 
 Region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
 MyInstance=`hostname`
+LOGFILE="/var/log/ec2-snapshot.log"
 
 export AWS_DEFAULT_REGION=${Region}
 
@@ -25,7 +26,7 @@ Snapshot=$(aws ec2 describe-snapshots \
   --output text | head -n1)
 
 if [ -z "$Snapshot" ]; then
-    aws ec2 create-snapshot --volume-id ${EC2_VolumeId} --tag-specifications 'ResourceType="snapshot",Tags=[{Key="Name",Value='$MyInstance'},{Key="Auto_SS",Value="true"}]' --description "Auto snapshot at root login" 2>>./snapshot.log
+    aws ec2 create-snapshot --volume-id ${EC2_VolumeId} --tag-specifications 'ResourceType="snapshot",Tags=[{Key="Name",Value='$MyInstance'},{Key="Auto_SS",Value="true"}]' --description "Auto snapshot at root login" 2>&1 >> ${LOGFILE}
     echo "Automatic snapshot was performed"
   else
     echo "Automatic snapshot does not run"

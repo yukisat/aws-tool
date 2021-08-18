23f0ffc57e1546e5ebc08a764b4fd0010422627e
yukisat <s.yuuki0120@gmail.com>
2021-08-19 08:06:32 +0900

    1日前のスナップショットがある場合削除する処理を追加

--- ec2-snapshot.sh
+++ ec2-snapshot.sh
@@ -4,10 +4,14 @@ set -ue
 
 Region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
 MyInstance=`hostname`
-LOGFILE="/var/log/ec2-snapshot.log"
+Target_Date=$(date "+%Y-%m-%dT%H:%M" -u -d '-1 hours')
+Delete_Date=$(date "+%Y-%m-%dT%H:%M" -u -d '-1 day')
+Now_Date=$(date "+%Y-%m-%dT%H:%M" -u)
+Log_File="/var/log/ec2-snapshot.log"
 
 export AWS_DEFAULT_REGION=${Region}
 
+## 対象インスタンスのボリュームIDを取得
 EC2_VolumeId=$( \
   aws ec2 describe-instances \
     --filter "Name=tag:Name,Values=${MyInstance}" \
@@ -15,18 +19,23 @@ EC2_VolumeId=$( \
     --output text \
 )
 
-## 1時間前のスナップショットが存在する場合作成しない
-TARGET_DATE=$(date "+%Y-%m-%dT%H:%M" -u -d '-1 hours')
-NOW_DATE=$(date "+%Y-%m-%dT%H:%M" -u)
+## 1日前のスナップショットは削除
+aws ec2 describe-snapshots \
+  --filters "Name=tag:Auto_SS,Values=true" \
+  --query "Snapshots[?(StartTime<='$Delete_Date')].[SnapshotId]" \
+  --output text | \
+  xargs -I{} aws ec2 delete-snapshot \
+  --snapshot-id {}
 
+## スナップショットの作成 (1時間以内にスナップショットが存在する場合作成しない)
 Snapshot=$(aws ec2 describe-snapshots \
   --filters "Name=tag:Auto_SS,Values=true" \
   --region ap-northeast-1 \
-  --query "Snapshots[?(StartTime>='$TARGET_DATE') && (StartTime<='$NOW_DATE')].[SnapshotId,StartTime]" \
+  --query "Snapshots[?(StartTime>='$Target_Date') && (StartTime<='$Now_Date')].[SnapshotId,StartTime]" \
   --output text | head -n1)
 
 if [ -z "$Snapshot" ]; then
-    aws ec2 create-snapshot --volume-id ${EC2_VolumeId} --tag-specifications 'ResourceType="snapshot",Tags=[{Key="Name",Value='$MyInstance'},{Key="Auto_SS",Value="true"}]' --description "Auto snapshot at root login" 2>&1 >> ${LOGFILE}
+    aws ec2 create-snapshot --volume-id ${EC2_VolumeId} --tag-specifications 'ResourceType="snapshot",Tags=[{Key="Name",Value='$MyInstance'},{Key="Auto_SS",Value="true"}]' --description "Auto snapshot at root login" 2>&1 >> ${Log_File}
     echo "Automatic snapshot was performed"
   else
     echo "Automatic snapshot does not run"

df632d4fb50c7e8ba63ac375d368666bbe970454
yuki sato (kaba) <35911121+yukisat@users.noreply.github.com>
2022-01-14 14:03:36 +0900

    Update ec2-delete-snapshot.sh
    タグの修正

--- ec2-delete-snapshot.sh
+++ ec2-delete-snapshot.sh
@@ -4,12 +4,13 @@ set -ue
 
 Region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
 Delete_Date=$(date "+%Y-%m-%dT%H:%M" -u -d '-3 day')
+InstanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 export AWS_DEFAULT_REGION=${Region}
 
 ## Auto_SS のタグ付きで3日前のスナップショットは削除
 aws ec2 describe-snapshots \
-  --filters "Name=tag:Auto_SS,Values=true" \
+  --filters "Name=tag:Auto_SS_TG,Values=${InstanceId}" \
   --query "Snapshots[?(StartTime<='$Delete_Date')].[SnapshotId]" \
   --output text | \
   xargs -I{} aws ec2 delete-snapshot \
